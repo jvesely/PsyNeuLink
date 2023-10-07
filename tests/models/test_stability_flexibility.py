6c56248990bbbd3149d52a51faf2881f73d83d02
Jan Vesely <jan.vesely@rutgers.edu>
2023-10-07 17:19:57 -0400

    WIP: stab flex model
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- tests/models/test_stability_flexibility.py
+++ tests/models/test_stability_flexibility.py
@@ -1,7 +1,7 @@
 import psyneulink as pnl
 import numpy as np
-import random
 import pytest
+
 
 # Define function to generate a counterbalanced trial sequence with a specified switch trial frequency
 def generateTrialSequence(N, Frequency):
@@ -70,12 +70,26 @@ def generateTrialSequence(N, Frequency):
 
 
 # Stability-Flexibility Model
-@pytest.mark.benchmark
-# ExecutionMode.LLVM takes too long to compile
-@pytest.mark.usefixtures("comp_mode_no_llvm")
+#@pytest.mark.stress
 @pytest.mark.parametrize("num_generators", [3,
                                             pytest.param(100000, marks=pytest.mark.stress)])
-def test_stability_flexibility(comp_mode, benchmark, num_generators):
+@pytest.mark.parametrize("mode", pytest.helpers.get_comp_execution_modes() +
+                                 [pytest.helpers.cuda_param('Python-PTX'),
+                                  pytest.param('Python-LLVM', marks=pytest.mark.llvm)])
+@pytest.mark.parametrize('prng', ['Default', 'Philox'])
+@pytest.mark.parametrize('fp_type', [pnl.core.llvm.ir.DoubleType, pnl.core.llvm.ir.FloatType])
+@pytest.mark.benchmark
+def test_stability_flexibility(mode, benchmark, num_generators, prng, fp_type):
+    if mode == pnl.ExecutionMode.LLVM:
+        pytest.skip("takes too long to compile")
+
+    if str(mode).startswith('Python-'):
+        ocm_mode = mode.split('-')[1]
+        mode = pnl.ExecutionMode.Python
+    else:
+        ocm_mode = 'Python'
+
+    pnl.core.llvm.builder_context.LLVMBuilderContext.default_float_ty = fp_type()
 
     # 16 is minimum number of trial inputs that can be generated
     taskTrain, stimulusTrain, cueTrain, correctResponse = generateTrialSequence(16, 0.5)
@@ -132,7 +146,10 @@ def test_stability_flexibility(comp_mode, benchmark, num_generators):
 
     # Control Mechanism Setting Cue-To-Stimulus Interval
     csiController = pnl.ControlMechanism(monitor_for_control=cueInterval,
-                                         control_signals=[(pnl.TERMINATION_THRESHOLD, controlModule)],
+                                         control_signals=pnl.ControlSignal(
+                                            modulates=(pnl.TERMINATION_THRESHOLD, controlModule),
+                                            default_allocation=1200
+                                         ),
                                          modulation=pnl.OVERRIDE)
 
     # Hadamard product of controlModule and Stimulus Information
@@ -169,12 +186,14 @@ def test_stability_flexibility(comp_mode, benchmark, num_generators):
                                           name='Scaled DDM Input')
 
     # Decision Module
-    decisionMaker = pnl.DDM(function=pnl.DriftDiffusionIntegrator(starting_point=STARTING_POINT,
+    decisionMaker = pnl.DDM(function=pnl.DriftDiffusionIntegrator(non_decision_time=STARTING_POINT,
                                                                   threshold=THRESHOLD,
-                                                                  noise=NOISE,
+                                                                  noise=np.sqrt(NOISE),
                                                                   time_step_size=0.001),
-                            reset_stateful_function_when=pnl.AtTrialStart(),
                             output_ports=[pnl.DECISION_VARIABLE, pnl.RESPONSE_TIME],
+                            reset_stateful_function_when=pnl.Never(),
+                            execute_until_finished=False,
+                            max_executions_before_finished=1000,
                             name='DDM')
 
     # Composition Creation
@@ -206,6 +225,12 @@ def test_stability_flexibility(comp_mode, benchmark, num_generators):
     stabilityFlexibility.add_projection(sender=ddmRecodeDrift, receiver=ddmInputScale)
     stabilityFlexibility.add_projection(sender=ddmInputScale, receiver=decisionMaker)
 
+    # Restrict non-mutable nodes to run only in the first pass
+#    stabilityFlexibility.scheduler.add_condition(taskLayer, pnl.AtPass(0))
+#    stabilityFlexibility.scheduler.add_condition(stimulusInfo, pnl.AtPass(0))
+#    stabilityFlexibility.scheduler.add_condition(correctResponseInfo, pnl.AtPass(0))
+#    stabilityFlexibility.scheduler.add_condition(congruenceWeighting, pnl.AtPass(0))
+
     # Hot-fix currently necessary to allow control module and DDM to execute in parallel in compiled mode
     # We need two gates in order to output both values (decision and response) from the ddm
     decisionGate = pnl.ProcessingMechanism(size=1, name="DECISION_GATE")
@@ -217,10 +242,6 @@ def test_stability_flexibility(comp_mode, benchmark, num_generators):
     stabilityFlexibility.add_projection(sender=decisionMaker.output_ports[0], receiver=decisionGate)
     stabilityFlexibility.add_projection(sender=decisionMaker.output_ports[1], receiver=responseGate)
 
-    # Sets scheduler conditions, so that the gates are not executed (and hence the composition doesn't finish) until decisionMaker is finished
-    stabilityFlexibility.scheduler.add_condition(decisionGate, pnl.WhenFinished(decisionMaker))
-    stabilityFlexibility.scheduler.add_condition(responseGate, pnl.WhenFinished(decisionMaker))
-
 
     # Add ObjectiveMechanism to store the values in 'saved_values'
     objectiveMech = pnl.ObjectiveMechanism(name="RESPONSE_Objective")
@@ -228,39 +249,92 @@ def test_stability_flexibility(comp_mode, benchmark, num_generators):
     stabilityFlexibility.add_node(objectiveMech)
     stabilityFlexibility.add_projection(sender=responseGate, receiver=objectiveMech)
     stabilityFlexibility.add_projection(sender=decisionGate, receiver=objectiveMech)
-    # combine decision and response time
+
+    # Combine decision and response time
+    # Response is either -0.2 or 0.2, multiply by 5 to get -1/1,
+    # then multiply by the response time to get a distribution of
+    # positive times for positive responses, and a distribution of
+    # negative times for negative responses.
     assert len(objectiveMech.input_ports) == 1
-    objectiveMech.input_port.function.operation = pnl.PRODUCT
-    # Response is either -0.2 or 0.2, multiply by 5 to get -1/1.
     objectiveMech.input_port.function.weights = [[5.], [1.]]
+    objectiveMech.input_port.function.operation = pnl.PRODUCT
 
     # Add controller to gather multiple samples
     stabilityFlexibility.add_controller(
         pnl.OptimizationControlMechanism(
             state_features=[taskLayer.input_port, stimulusInfo.input_port, cueInterval.input_port, correctResponseInfo.input_port],
+            function=pnl.GridSearch(save_values=True),
             agent_rep=stabilityFlexibility,
             objective_mechanism=objectiveMech,
+            comp_execution_mode=ocm_mode,
             control_signals=pnl.ControlSignal(
                 modulates=('seed-function', decisionMaker),
                 modulation=pnl.OVERRIDE,
+                default_allocation=[num_generators],
                 allocation_samples=pnl.SampleSpec(start=0, stop=num_generators - 1, step=1),
                 cost_options=pnl.CostFunctions.NONE
             )
         )
     )
+    if prng == 'Philox':
+        stabilityFlexibility.controller.function.parameters.random_state.set(pnl.core.globals.utilities._SeededPhilox([0]))
+        decisionMaker.parameters.random_state.set(pnl.core.globals.utilities._SeededPhilox([0]))
+        decisionMaker.function.parameters.random_state.set(pnl.core.globals.utilities._SeededPhilox([0]))
+
+    # Set scheduler conditions so that the gates are not executed (and hence the composition doesn't finish) until decisionMaker is finished
+    stabilityFlexibility.scheduler.add_condition(decisionGate, pnl.WhenFinished(decisionMaker))
+    stabilityFlexibility.scheduler.add_condition(responseGate, pnl.WhenFinished(decisionMaker))
 
     inputs = {taskLayer: taskTrain,
               stimulusInfo: stimulusTrain,
               cueInterval: cueTrain,
               correctResponseInfo: correctResponse}
 
-    benchmark(stabilityFlexibility.run, inputs, execution_mode=comp_mode, num_trials=1)
+    benchmark(stabilityFlexibility.run, inputs, execution_mode=mode, num_trials=1)
 
-    print(stabilityFlexibility.results[0])
-    print(stabilityFlexibility.controller.function.saved_values)
     if num_generators == 3:
-        assert np.allclose(stabilityFlexibility.results[0], [[1200.], [0.2], [0.821]])
+
         # saved values are only available in Python, and are overwritten after every invocation
-        if comp_mode == pnl.ExecutionMode.Python and not benchmark.enabled:
-            assert np.allclose(np.squeeze(stabilityFlexibility.controller.function.saved_values),
-                               [0.821, 0.662, -0.499])
+        if mode == pnl.ExecutionMode.Python and not benchmark.enabled:
+            is_fp32 = ocm_mode != 'Python' and fp_type.intrinsic_name.endswith('32')
+            ocm_results = np.squeeze(stabilityFlexibility.controller.function.saved_values)
+
+            if prng == 'Default' and is_fp32:
+                np.testing.assert_allclose(ocm_results, [0.07699998, 0.26600012, 0.05999995])
+            elif prng == 'Default':   # fp64 computation
+                np.testing.assert_allclose(ocm_results, [0.077, 0.266, 0.06])
+            elif prng == 'Philox' and is_fp32:
+                np.testing.assert_allclose(ocm_results, [0.18600021, 0.10100003, 0.19800022])
+            elif prng == 'Philox':   # fp64 computation
+                np.testing.assert_allclose(ocm_results, [0.235, 0.086, 0.229])
+            else:
+                assert False, "Unknown PRNG and fp_type combination: {} {}".format(prng, str(fp_type))
+
+        # The final ('max') results are different for the special case of mode == Python and
+        # ocm_mode != Python. The OCM simulation is run in compiled mode using fp32, this
+        # selects a "winning" set of parameters. Those parameters are then used to run the
+        # composition in Python using fp64 precision.
+        # This is particularly the case for Philox which produces different sequences for fp32
+        # and fp64, even when using the same seed.
+        is_fp32 = mode != pnl.ExecutionMode.Python and fp_type.intrinsic_name.endswith('32')
+        if prng == 'Default' and is_fp32:
+            np.testing.assert_allclose(stabilityFlexibility.results[0], [[1200.], [0.2], [0.26600012]])
+        elif prng == 'Default':   # fp64 computation
+            np.testing.assert_allclose(stabilityFlexibility.results[0], [[1200.], [0.2], [0.266]])
+        elif prng == 'Philox' and is_fp32:
+            np.testing.assert_allclose(stabilityFlexibility.results[0], [[1200.], [0.2], [0.19800022]])
+        elif prng == 'Philox' and ocm_mode != 'Python' and fp_type.intrinsic_name.endswith('32'):
+            # A special case of running fp32 simulations and fp64 final execution. It selects the
+            # fp64 result at the same index (using the same seed) as the largest fp32 result (see above).
+            np.testing.assert_allclose(stabilityFlexibility.results[0], [[1200.], [0.2], [0.229]])
+        elif prng == 'Philox':   # fp64 computation
+            np.testing.assert_allclose(stabilityFlexibility.results[0], [[1200.], [0.2], [0.235]])
+        else:
+            assert False, "Unknown PRNG and fp_type combination: {} - {}".format(prng,  fp_type.intrinsic_name)
+
+    if num_generators == 100000 and mode == pnl.ExecutionMode.Python:
+        data = stabilityFlexibility.controller.function.saved_values
+
+        from matplotlib import pyplot as plt
+        plt.hist(data, bins=600, range=[-3.0,3.0])
+#        plt.show()
